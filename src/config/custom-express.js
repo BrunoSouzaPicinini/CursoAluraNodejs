a7f0d629bc066b961f14994ac0578df9def34981
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2019-06-09 14:09:44 -0300

    css e bootstrap casadocodigo

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -6,19 +6,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-//middleware para arquivos estáticos
-app.use('/estatico', express.static('src/app/public/'));
+app.use('/estatico', express.static('src/app/public'));
 
-//habilitando middleware bodyParser para re ceber objetos complexos em formato JSON
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(methodOverride(function (req, res) {
-    if(req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._method;
-        delete req.body._method;
-        return method;
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
     }
 }));
 
