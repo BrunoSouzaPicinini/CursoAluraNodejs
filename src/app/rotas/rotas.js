f1b4aa6d919ce142bebc5146d92f11beac0006f2
Bruno de Souza Picinini <bruno.picinini@objective.com.br>
2019-06-13 18:32:25 -0300

    retornar dados parciais para pagina com erro

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -54,11 +54,10 @@ module.exports = (app) => {
             return resp.marko(
                 require('../views/livros/form/form.marko'),
                 { 
-                    livro: {},
+                    livro: req.body,
                     errosValidacao: erros.array()
                 }
             );
-        
         }
 
         livroDao.adiciona(req.body)
